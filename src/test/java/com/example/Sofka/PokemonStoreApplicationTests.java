5b6a0128211faac43576f47f41897974674afe49
LuisaBAvila <luisa.barragan@sofka.com.co>
2023-01-17 15:56:14 -0500

    feat: se inicializa proyecto

---
+++ src/test/java/com/example/Sofka/PokemonStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sofka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
